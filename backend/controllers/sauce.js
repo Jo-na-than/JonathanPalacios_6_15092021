e389f2fd53017ccf92236156facc0c9246617d70
Jo-na-than <jonathan.palacios@orange.fr>
2021-09-29 15:04:03 +0200

    correction + ajout com files controllers

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,20 +1,26 @@
 // Récupération du "models/Sauce.js"
 const Sauce = require('../models/Sauce');
 
-// Récupération du module 'file system' de Node permettant de gérer les téléchargements et modifications d'images
+// Récupération du module 'file system' de Node permettant de gérer les téléchargements,la modifications d'images et la suppression de tout le fichier
 const fs = require('fs');
 
 // Lecture de toutes les sauces de la BD
 exports.getAllSauces = (req, res, next) => {
+  // On utilise la méthode find pour obtenir la liste complète des sauces trouvées dans la base
     Sauce.find()
+      // Si OK on retourne un tableau de toutes les données
       .then(sauces => res.status(200).json(sauces))
+      // Si erreur on retourne un message d'erreur
       .catch(error => res.status(400).json({ error }));
 };
 
 // Lecture d'une sauce avec son ID (Get/:id)
 exports.getOneSauce = (req, res, next) => {
+   // On utilise la méthode findOne et on lui passe l'objet de comparaison
     Sauce.findOne({ _id: req.params.id })
+      // Si ok on retourne une réponse et l'objet
       .then(sauce => res.status(200).json(sauce))
+       // Si erreur on génère une erreur 404 pour dire qu'on ne trouve pas l'objet
       .catch(error => res.status(404).json({ error }));
 };
 
@@ -34,27 +40,43 @@ exports.createSauce = (req, res, next) => {
       .catch(error => res.status(400).json({ error }));
 };
 
-// Modification d'une sauce (Update)
+// Modification d'une sauce et suppression image (Update)
 exports.modifySauce = (req, res, next) => {
-    const sauceObject = req.file ?
-      // Si il existe déjà une image
-      {
-        ...JSON.parse(req.body.sauce),
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-      } : { ...req.body }; 
-      // Si il n'existe pas l'image
-      Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+  if(req.file){
+    Sauce.findOne({_id: req.params.id})
+    .then(sauce => {
+      // On supprime l'ancienne image du serveur
+      const filename = sauce.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, () =>{
+        const sauceObject = {
+          // On modifie les données et on ajoute la nouvelle image
+           ...JSON.parse(req.body.sauce),  
+           imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        }
+        Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Objet modifié !'}))
         .catch(error => res.status(400).json({ error }));
+      })
+    })
+    .catch(error => res.status(500).json({ error }));
+
+  }else{
+    const sauceObject = { ...req.body };
+    // On applique les paramètre de sauceObject
+    Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+  }
 };
 
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({_id: req.params.id})
       .then(sauce => {
-        // Récupération du nom du fichier
+        // Pour extraire ce fichier, on recupere l'url de la sauce, et on le split autour de la chaine de caractères, donc le nom du fichier
         const filename = sauce.imageUrl.split('/images/')[1];
         // On efface le fichier (unlink)
         fs.unlink(`images/${filename}`, () => {
+          // On supprime le document correspondant de la base de donnees
           Sauce.deleteOne({ _id: req.params.id })
           .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
           .catch(error => res.status(400).json({ error }));
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,23 +1,24 @@
-// Utilisation de BCRYPT pour hasher le mot de passe des utilisateurs
+// On utilise BCRYPT pour hasher le mot de passe des utilisateurs
 const bcrypt = require('bcrypt')
 
-// Récupération de notre model/user.js, créer avec le schéma Mongoose
-const user = requires('../models/user');
+// On recupere notre models/User.js, créer avec le schéma MONGOOSE
+const User = require('../models/user');
 
-// Utilisation du JSONWEBTOKEN pour attribuer un token au moment de la connexion
+// On utilise jsonwebtoken pour attribuer un token au moment de la connexion
 const jwt = require('jsonwebtoken');
 
-// Création de nouveaux utilisateurs (Post signup)
+
+// Création de nouveaux utilisateurs dans la BD (Post signup)
 exports.signup = (req, res, next) => {
-    // Hash du mot de passe avec bcrypt
+    // Hash du mot de passe avec bcrypt et 10 tours pour creer mot de passe securise
     bcrypt.hash(req.body.password, 10)
     .then(hash => {
-        // Création du nouvel utilisateur
+        // Création du nouvel utilisateur avec mot de passe crypté (HASH)
         const user = new User({
             email: req.body.email,
             password: hash
         });
-        // Sauvegarde de l'utilisateur dans la BD
+        // Sauvegarde de l'utilisateur USER.SAVE dans la BD
         user.save()
         .then(() => res.status(201).json({ message: 'Utilisateur créé !'}))
         .catch(error => res.status(400).json({ error }));
@@ -27,30 +28,31 @@ exports.signup = (req, res, next) => {
 
 // Création de la connexion d'utilisateur enregistré (Post login)
 exports.login = (req, res, next) => {
-    // Recherche d'un utilisateur dans la BD
-     User.findOne({ email: req.body.email })
-     .then(user => {
-         // Si on ne trouve pas l'utilisateur
-         if(!user) {
-             return res.status(401).json({ error: 'Utilisateur non trouvé !'})
-         }
-         // On compare le mot de passe de la requete avec celui de la base de données
-         bcrypt.compare(req.body.password, user.password)
-         .then(valid => {
-             if(!valid) {
-                 return res.status(401).json({ error: 'Mot de passe incorrect !'})
-             }
-             res.status(200).json({
-                 userId: user._id,
-                 // Création d'un token pour sécuriser le compte de l'utilisateur
-                 token: jwt.sign(
-                     { userId: user._id },
-                     'RANDOM_TOKEN_SECRET',
-                     { expiresIn: '24h' }
-                 )
-             });
-         })
-         .catch(error => res.status(500).json({ error }));
-     })
-     .catch(error => res.status(500).json({ error }));
- };
+   // Recherche d'un utilisateur dans la BD
+    User.findOne({ email: req.body.email })
+    .then(user => {
+        // Si on ne trouve pas l'utilisateur on renvoi une erreur
+        if(!user) {
+            return res.status(401).json({ error: 'Utilisateur non trouvé !'})
+        }
+        // On compare le mot de passe de la requete avec le HASH enregistrer dans le user grace a BCRYPT
+        bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+            if(!valid) {
+            // Si la comparaison n'est pas bonne cela envoie une erreur
+                return res.status(401).json({ error: 'Mot de passe incorrect !'})
+            }
+            res.status(200).json({
+                userId: user._id,
+                // Création d'un token pour sécuriser le compte de l'utilisateur
+                token: jwt.sign(
+                    { userId: user._id },
+                    'RANDOM_TOKEN_SECRET',
+                    { expiresIn: '24h' }
+                )
+            });
+        })
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
